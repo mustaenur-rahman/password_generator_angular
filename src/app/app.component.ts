32b789ad6eea110baf10f9deb3e78c7c6206f22b
Md. Musta-E-Nur Rahman <md.musta-e-nur.rahman@b-architects.com>
2022-07-14 15:00:58 +0900

    Completed password generator app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
   password = '';
   useLetters = false;
   useNumbers = false;
   useSymbols = false;
+
   onButtonClick(){
-    console.log(this.useLetters);
-    console.log(this.useNumbers);
-    console.log(this.useSymbols);
-    this.password = 'my password !!';
+    // console.log(`
+    //   generating password with these properties:
+    //   Include letters: ${this.useLetters}
+    //   Include Numbers: ${this.useNumbers}
+    //   Include Symbols: ${this.useSymbols}
+    // `);
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.useLetters){
+      validChars += letters;
+    }
+    
+    if(this.useNumbers){
+      validChars += numbers;
+    }
+
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+    let generatedPassword = '';
+    for(let i=0;i<this.length;i++){
+      let index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+
   }
 
   onChangeUseLetters(){
@@ -27,5 +55,15 @@ export class AppComponent {
 
   onChangeUseSymbols(){
     this.useSymbols = !this.useSymbols;
+  }
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+      console.log(this.length);
+    }else{
+      this.length = 0;
+    }
   }
 }
